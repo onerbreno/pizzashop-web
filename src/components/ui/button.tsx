eb0bbd39e4bc1f14d26dca4875de27e16d6e453e
Breno <onerbreno@outlook.com>
2024-01-20 22:05:17 -0300

    chore Add eslint + prettier

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,4 +1,4 @@
-import { Slot } from "@radix-ui/react-slot";
+reimport { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import * as React from "react";
 
