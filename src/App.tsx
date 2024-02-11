14a0d1099f082599a9c577d39155ea1a42dd5f67
Breno <onerbreno@outlook.com>
2024-02-10 22:37:51 -0300

    feat: Add react helmet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import "./global.css";
+import './global.css'
 
-import { RouterProvider } from "react-router-dom";
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
 
-import { router } from "./routes";
+import { router } from './routes'
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }
